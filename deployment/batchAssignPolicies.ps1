0d854a5ca3c1ce2cdf2490418237f37eb4163145
Scott Rutz <scrutz@microsoft.com>
2019-12-31 13:41:28 -0600

    cp

--- deployment/batchAssignPolicies.ps1
+++ deployment/batchAssignPolicies.ps1
@@ -2,6 +2,7 @@ $policyDefRootFolder = "$Env:ROOT_FOLDER/policydefinitions"
 $subscriptionId = "$Env:SUBSCRIPTION_ID"
 $releaseEnvironment = "$Env:RELEASE_ENVIRONMENT"
 
+
 Write-Host policyDefRootFolder: $policyDefRootFolder
 Write-Host subscriptionId: $subscriptionId
 Write-Host releaseEnvironment: $releaseEnvironment
@@ -10,7 +11,10 @@ foreach ($policyDefFolder in (Get-ChildItem -Path $policyDefRootFolder -Director
 
     Write-Host Processing folder: $policyDefFolder.Name
     $policy = Get-AzPolicyDefinition -Name $policyDefFolder.Name
-    Write-Host Creating assignment for: $policy
+    $policyParamater = "$($policyDefFolder.FullName)\values.($releaseEnvironment).json"
 
-    #New-AzPolicyAssignment -Name $policyDefFolder.Name -PolicyDefinition $policy -Scope $subscriptionId -PolicyParameter "$($policyDefFolder.FullName)\values.($releaseEnvironment).json"
+    Write-Host Creating assignment for: $policy
+    Write-Host Creating assignment for: $policyParamater
+
+    New-AzPolicyAssignment -Name $policyDefFolder.Name -PolicyDefinition $policy -Scope $subscriptionId -PolicyParameter $policyParamater
 }
