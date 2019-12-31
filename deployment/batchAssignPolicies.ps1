35403a414c0a8e690d55386dd0babf67d3f4b33f
Scott Rutz <scrutz@microsoft.com>
2019-12-31 13:43:22 -0600

    cp

--- deployment/batchAssignPolicies.ps1
+++ deployment/batchAssignPolicies.ps1
@@ -17,4 +17,5 @@ foreach ($policyDefFolder in (Get-ChildItem -Path $policyDefRootFolder -Director
     Write-Host Creating assignment for: $policyParamater
 
     New-AzPolicyAssignment -Name $policyDefFolder.Name -PolicyDefinition $policy -Scope $subscriptionId -PolicyParameter $policyParamater
+    
 }
